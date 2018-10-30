ebc2a582bf850266d96e7d05bc93e07fdd63f4e6
EVIL GOFRE <danieltorresingles@gmail.com>
2018-10-30 23:48:41 +0100

    First Commit

---
+++ Assets/Scripts/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PersonController : MonoBehaviour
+{
+	private NavMeshAgent _agent;
+	
+	[SerializeField] private GameObject _target;
+	private Vector3 _destination;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_destination = _target.transform.position;
+		_agent.SetDestination(_destination);
+	}
+}
